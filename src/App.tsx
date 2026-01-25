f2b6842ce83e13a268e3d1d6160fff3b93f47f47
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-25 21:35:05 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="p-10 bg-blue-500 text-white">
+      <h1 className="text-3xl font-bold">Tailwind v4 is working!</h1>
+    </div>
+  );
+}
+
+export default App;

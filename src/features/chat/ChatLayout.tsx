a93ac1d6ca40782efe530d5a62630f338ea31b02
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-02-11 09:49:17 +0530

    chat window component created and added in the wrapper with dummy data

--- src/features/chat/ChatLayout.tsx
+++ src/features/chat/ChatLayout.tsx
@@ -1,6 +1,12 @@
+import ChatWindow from "./ChatWindow";
+import Sidebar from "./Sidebar";
+
 const ChatLayout = () => {
   return (
-    <div className="flex h-screen w-full overflow-hidden bg-[var(--bg-deep)] font-sans"></div>
+    <div className="flex h-screen w-full overflow-hidden bg-[var(--bg-deep)] font-sans">
+      <Sidebar />
+      <ChatWindow />
+    </div>
   );
 };
 
